5fc2fd0bbdb6ee6995d656c8af40b799429e28e4
antoni <janikantoni@poczta.fm>
2026-05-09 19:37:03 +0200

    update updateWeb.sh

--- updateWeb.sh
+++ updateWeb.sh
@@ -10,7 +10,7 @@ git checkout --orphan web-build
 git add -f index.html index.js index.wasm index.data
 
 # 4. Utwórz commit (będzie to jedyny commit na tej gałęzi)
-git commit -m "Aktualizacja wersji webowej"
+git commit -m "Web version update"
 
 # 5. Wypchnij na GitHuba z flagą -f (force).
 # To nadpisze całą gałąź 'web-build' na serwerze, kasując poprzedni build i jego historię.
@@ -18,6 +18,8 @@ git push -f origin web-build
 
 # 6. Wróć na swoją główną gałąź (jeśli używasz 'master' zamiast 'main', zmień to poniżej)
 git checkout eq
+
+rm index.*
 
 # 7. Usuń gałąź 'web-build' z dysku lokalnego.
 # Dzięki temu przy następnym uruchomieniu skryptu krok 2 znów zadziała poprawnie.
